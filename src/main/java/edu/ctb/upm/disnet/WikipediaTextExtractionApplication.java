453c33553e40aa8345f790f26223fbe60a85c8a0
Gerardo Lagunes García <gerardolagar@ctb.upm.es>
2018-02-09 15:55:19 +0100

    Initial commit

---
+++ src/main/java/edu/ctb/upm/disnet/WikipediaTextExtractionApplication.java
@@ -0,0 +1,12 @@
+package edu.ctb.upm.disnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikipediaTextExtractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikipediaTextExtractionApplication.class, args);
+	}
+}
