c366def6c3c570092836e446fbef975191db5b82
Gerardo Lagunes García <gerardolagar@ctb.upm.es>
2018-08-18 01:49:08 +0200

    Actualización que permite retornar un JSON no serializado, se modificó la clase de Table para que esté compuesta por una lista de String no de objetos "Tr" y sea eficiente leer el JSON de respuesta (2).

--- src/main/java/edu/ctb/upm/midas/model/ResponseJSON.java
+++ src/main/java/edu/ctb/upm/midas/model/ResponseJSON.java
@@ -1,4 +1,12 @@
 package edu.ctb.upm.midas.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import edu.ctb.upm.midas.model.document_structure.SourceJSON;
+import edu.ctb.upm.midas.model.document_structure.code.Resource;
+
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by gerardo on 14/03/2018.
  *
@@ -9,4 +17,77 @@ package edu.ctb.upm.midas.model;
  * @see
  */
 public class ResponseJSON {
+
+    private String responseCode;
+    private String responseMessage;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private List<SourceJSON> sources;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private HashMap<String, Resource> resourceHashMap;
+    private String start_time;
+    private String end_time;
+
+
+    public String getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(String responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+
+    public List<SourceJSON> getSources() {
+        return sources;
+    }
+
+    public void setSources(List<SourceJSON> sources) {
+        this.sources = sources;
+    }
+
+    public HashMap<String, Resource> getResourceHashMap() {
+        return resourceHashMap;
+    }
+
+    public void setResourceHashMap(HashMap<String, Resource> resourceHashMap) {
+        this.resourceHashMap = resourceHashMap;
+    }
+
+    public String getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(String start_time) {
+        this.start_time = start_time;
+    }
+
+    public String getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(String end_time) {
+        this.end_time = end_time;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ResponseJSON{\n" +
+                "responseCode='" + responseCode + '\'' + "\n" +
+                ", responseMessage='" + responseMessage + '\'' + "\n" +
+                ", sourceCount=" + sources.size() + "\n" +
+                ", sources=" + sources + "\n" +
+                ", resourceHashMap=" + resourceHashMap + "\n" +
+                ", start_time='" + start_time + '\'' + "\n" +
+                ", end_time='" + end_time + '\'' + "\n" +
+                '}';
+    }
+
 }
